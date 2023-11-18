ccc460caa7962ccc9680ecca59880ac6ab01104e
SAKTHI M <msakthi150@gmail.com>
2023-11-18 09:58:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [transformation, setTransformation] = useState('Kaioken');
+
+  const toggleTransformation = () => {
+    setTransformation((currentTransformation) =>
+      currentTransformation === 'Kaioken' ? 'SuperSaiyan' : 'Kaioken'
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Goku's Transformation: {transformation}</h1>
+      <button onClick={toggleTransformation}>Toggle Transformation</button>
     </div>
   );
-}
+};
 
 export default App;
